8e8ed97b0cddbf9e1932e009e85e8f8d4801155c
AndreaReynoso132 <95582384+AndreaReynoso132@users.noreply.github.com>
2023-06-23 01:49:35 -0300

    Comenzamos el proyecto en Python

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # ProyectoFinal-TercerSemestre-OldSchool
+
+
+-Sistema de Órdenes de Compras-
+
+Este es un sistema básico de órdenes de compras desarrollado en Python utilizando la biblioteca Tkinter para la interfaz gráfica y SQLite para la base de datos.
+
+El sistema permite a los usuarios ingresar órdenes de compras, visualizar la lista de órdenes ingresadas, calcular el total de gastos y generar informes o reportes basados en las órdenes de compra almacenadas en la base de datos.
+
+-Requisitos-
+Python 3.x
+Tkinter (incluido en la mayoría de las instalaciones de Python)
+SQLite (incluido en la mayoría de las instalaciones de Python)
+Pillow (biblioteca de Python para el manejo de imágenes)
+
+-Instalación y Ejecución-
+Clona o descarga este repositorio en tu máquina local.
+
+Abre una terminal y navega hasta el directorio del proyecto.
+
+Crea y activa un entorno virtual (opcional pero se recomienda).
+
+Instala las dependencias requeridas ejecutando el siguiente comando:
+pip install -r requirements.txt
+
+Ejecuta el programa utilizando el siguiente comando:
+python orden_compra.py
+
+-Características-
+
+Inicio de Sesión: El programa solicita al usuario que ingrese un nombre de usuario y una contraseña para acceder al sistema. Actualmente, se utiliza una combinación fija de usuario y contraseña para la demostración.
+
+Ventana Principal: Después de iniciar sesión correctamente, se abre la ventana principal del sistema. Aquí se pueden ingresar órdenes de compras, visualizar la lista de órdenes ingresadas, calcular el total de gastos, generar informes y salir del programa.
+
+Ingreso de Órdenes de Compra: Se pueden ingresar órdenes de compras especificando el producto, la cantidad y el precio. Al hacer clic en el botón "Agregar Orden", se guarda la orden en la base de datos y se actualiza la lista de órdenes y el total de gastos.
+
+Eliminación de Órdenes de Compra: Se puede seleccionar una orden de compra de la lista y hacer clic en el botón "Eliminar Producto" para eliminarla de la base de datos y de la lista visualmente. El total de gastos se actualiza automáticamente.
+
+Generación de Informes: Se puede generar un informe o reporte basado en las órdenes de compra almacenadas en la base de datos. Actualmente, el informe muestra una tabla con los detalles de todas las órdenes ingresadas.
+
+Salir del Programa: Se puede hacer clic en el botón "Salir" para cerrar el programa.
+
+-Personalización-
+
+Colores y Fuentes: Los colores de fondo, los colores de texto y las fuentes utilizadas en la interfaz gráfica se pueden personalizar modificando los parámetros correspondientes en el código.
+
+Tamaño del Icono: El tamaño del icono se puede ajustar creando un nuevo archivo de icono con las dimensiones deseadas utilizando una herramienta de edición de imágenes y reemplazando el archivo "icono.ico" en el directorio del proyecto.
+
+-Contribución-
+
+Si deseas contribuir a este proyecto, siéntete libre de enviar pull requests o abrir issues con tus sugerencias o mejoras.
+
+Esperamos que esta guía README te ayude a entender y utilizar el sistema de órdenes de compras. Si tienes alguna pregunta adicional, no dudes en contactarnos.
+
+¡Gracias por utilizar el sistema de órdenes de compras de Old School!
+
+![Logo OldSchool](oldschool.png)
---
+++ orden_compra.py
@@ -0,0 +1,229 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+import sqlite3
+from tkinter import messagebox
+from reportlab.pdfgen import canvas
+
+def verificar_credenciales():
+    usuario = usuario_entry.get()
+    contrasena = contrasena_entry.get()
+
+    if usuario == "admin" and contrasena == "admin123":
+        ventana_login.destroy()
+        abrir_ventana_principal()
+    else:
+        messagebox.showerror("Error", "Contraseña/usuario incorrecto")
+
+def abrir_ventana_principal():
+    ventana_principal = tk.Tk()
+    ventana_principal.title("Sistema de Órdenes de Compras")
+    ventana_principal.configure(bg="DarkOliveGreen")
+
+    ventana_principal.iconbitmap("Orden-de.ico") # Icono
+
+    conexion = sqlite3.connect("ordenes_compra.db")
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS ordenes_compra (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            producto TEXT,
+            cantidad INTEGER,
+            precio REAL
+        )
+    """)
+    conexion.commit()
+
+    def agregar_orden():
+        producto = producto_entry.get()
+        cantidad = cantidad_entry.get()
+        precio = precio_entry.get()
+
+        if producto and cantidad and precio:
+            cantidad = int(cantidad)
+            precio = float(precio)
+
+            cursor.execute("INSERT INTO ordenes_compra (producto, cantidad, precio) VALUES (?, ?, ?)", (producto, cantidad, precio))
+            conexion.commit()
+
+            actualizar_lista()
+            calcular_total()
+
+            producto_entry.delete(0, tk.END)
+            cantidad_entry.delete(0, tk.END)
+            precio_entry.delete(0, tk.END)
+        else:
+            messagebox.showwarning("Advertencia", "Por favor completa todos los campos.")
+
+    def eliminar_orden():
+        # Obtener el índice de la orden seleccionada en la lista
+        seleccion = lista_orden.curselection()
+
+        # Verificar si se seleccionó una orden
+        if seleccion:
+            # Obtener la orden seleccionada
+            orden_seleccionada = lista_orden.get(seleccion)
+
+            # Obtener el producto de la orden seleccionada
+            producto = orden_seleccionada.split(' (')[0]
+
+            # Eliminar la orden de la base de datos
+            cursor.execute("DELETE FROM ordenes_compra WHERE producto=?", (producto,))
+            conexion.commit()
+
+            # Eliminar la orden de la lista visual
+            lista_orden.delete(seleccion)
+
+            # Actualizar el total
+            calcular_total()
+        else:
+            messagebox.showwarning("Error", "No se ha seleccionado ninguna orden.")
+
+
+    def actualizar_lista():
+        cursor.execute("SELECT * FROM ordenes_compra")
+        ordenes = cursor.fetchall()
+
+        lista_orden.delete(0, tk.END)
+
+        for orden in ordenes:
+            producto = orden[1]
+            cantidad = orden[2]
+            precio = orden[3]
+            lista_orden.insert(tk.END, f"{producto} ({cantidad}) - ${precio:.2f}")
+
+    def calcular_total():
+        total = 0
+
+        for i in range(lista_orden.size()):
+            orden = lista_orden.get(i)
+            precio_inicio = orden.rfind("$") + 1
+            precio = float(orden[precio_inicio:])
+            total += precio
+
+        total_label.config(text=f"Total: ${total:.2f}")
+
+    def generar_informe():
+        # Conexión a la base de datos
+        conexion = sqlite3.connect("ordenes_compra.db")
+        cursor = conexion.cursor()
+
+        # Obtener los datos de las órdenes de compra
+        cursor.execute("SELECT * FROM ordenes_compra")
+        ordenes = cursor.fetchall()
+
+        # Crear el documento PDF
+        c = canvas.Canvas("informe_ordenes.pdf")
+
+        # Título del informe
+        c.setFont("Helvetica-Bold", 16)
+        c.drawString(50, 750, "Informe de Órdenes de Compra")
+        c.setFont("Helvetica", 12)
+
+        # Contenido del informe
+        y = 700  # Posición vertical inicial
+
+        # Crear una lista para almacenar los datos de las órdenes
+        datos_orden = []
+
+        for orden in ordenes:
+            producto = orden[1]
+            cantidad = orden[2]
+            precio = orden[3]
+
+            # Agregar los datos de la orden a la lista
+            datos_orden.append(f"{producto} ({cantidad}) - ${precio:.2f}")
+
+        # Imprimir los datos de las órdenes en el informe
+        for dato in datos_orden:
+            c.drawString(50, y, dato)
+            y -= 20
+
+        # Guardar y cerrar el documento PDF
+        c.save()
+
+        messagebox.showinfo("Informe generado", "Se ha generado el informe de órdenes de compra.")
+
+    # Interfaz de usuario
+    titulo_label = tk.Label(ventana_principal, text="Sistema de Órdenes de Compras", font=("Helvetica", 18), bg="DarkOliveGreen", fg="white")
+    titulo_label.pack(pady=10)
+
+    # Marco para ingresar los datos de la orden
+    marco_orden = tk.LabelFrame(ventana_principal, text="Nueva Orden de Compra", bg="DarkOliveGreen", fg="white", padx=20, pady=20)
+    marco_orden.pack(padx=10, pady=10)
+
+    # Campos de entrada de datos
+    producto_label = tk.Label(marco_orden, text="Producto:", bg="DarkOliveGreen", fg="white")
+    producto_label.grid(row=0, column=0)
+    producto_entry = tk.Entry(marco_orden)
+    producto_entry.grid(row=0, column=1)
+
+    cantidad_label = tk.Label(marco_orden, text="Cantidad:", bg="DarkOliveGreen", fg="white")
+    cantidad_label.grid(row=1, column=0)
+    cantidad_entry = tk.Entry(marco_orden)
+    cantidad_entry.grid(row=1, column=1)
+
+    precio_label = tk.Label(marco_orden, text="Precio:", bg="DarkOliveGreen", fg="white")
+    precio_label.grid(row=2, column=0)
+    precio_entry = tk.Entry(marco_orden)
+    precio_entry.grid(row=2, column=1)
+
+    agregar_button = tk.Button(marco_orden, text="Agregar Orden", command=agregar_orden)
+    agregar_button.grid(row=3, columnspan=2, pady=10)
+
+    # Marco para mostrar la lista de órdenes
+    marco_lista = tk.LabelFrame(ventana_principal, text="Lista de Órdenes de Compra", bg="DarkOliveGreen", fg="white", padx=20, pady=20)
+    marco_lista.pack(padx=10, pady=10)
+
+    lista_orden = tk.Listbox(marco_lista, width=50)
+    lista_orden.pack()
+
+    eliminar_button = tk.Button(marco_lista, text="Eliminar Orden", command=eliminar_orden)
+    eliminar_button.pack(pady=10)
+
+    # Etiqueta para mostrar el total
+    total_label = tk.Label(ventana_principal, text="Total: $0.00", font=("Helvetica", 14), bg="DarkOliveGreen", fg="white")
+    total_label.pack(pady=10)
+
+    # Botón para generar el informe
+    informe_button = tk.Button(ventana_principal, text="Generar Informe", command=generar_informe)
+    informe_button.pack(pady=10)
+
+    # Cargar las órdenes existentes al iniciar la aplicación
+    actualizar_lista()
+    calcular_total()
+    
+    ventana_principal.mainloop()
+
+
+
+
+ventana_login = tk.Tk()
+ventana_login.title("Inicio de Sesión")
+ventana_login.geometry("400x400")
+ventana_login.configure(bg="DarkOliveGreen")
+
+
+
+
+background_image = ImageTk.PhotoImage(Image.open("oldschool.png"))
+background_label = tk.Label(ventana_login, image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+
+ventana_login.iconbitmap("Orden-de.ico") # Icono
+
+usuario_label = tk.Label(ventana_login, text="Usuario:", bg="DarkOliveGreen", fg="white")
+usuario_label.pack(pady=9)
+usuario_entry = tk.Entry(ventana_login)
+usuario_entry.pack(pady=5)
+
+contrasena_label = tk.Label(ventana_login, text="Contraseña:", bg="DarkOliveGreen", fg="white")
+contrasena_label.pack(pady=10)
+contrasena_entry = tk.Entry(ventana_login, show="*")
+contrasena_entry.pack(pady=5)
+
+login_button = tk.Button(ventana_login, text="Iniciar sesión", command=verificar_credenciales)
+login_button.pack(pady=10)
+
+ventana_login.mainloop()
