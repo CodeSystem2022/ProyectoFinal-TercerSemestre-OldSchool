3c3606aaeaa5d4d8bb5ee011e886140b404051b8
AndreaReynoso132 <95582384+AndreaReynoso132@users.noreply.github.com>
2023-06-23 18:05:44 -0300

    Desarrollo de la funcionalidad de autenticación

--- orden_compra.py
+++ orden_compra.py
@@ -3,3 +3,13 @@ from PIL import ImageTk, Image
 import sqlite3
 from tkinter import messagebox
 from reportlab.pdfgen import canvas
+
+def verificar_credenciales():
+    usuario = usuario_entry.get()
+    contrasena = contrasena_entry.get()
+
+    if usuario == "admin" and contrasena == "admin123":
+        ventana_login.destroy()
+        abrir_ventana_principal()
+    else:
+        messagebox.showerror("Error", "Contraseña/usuario incorrecto")
